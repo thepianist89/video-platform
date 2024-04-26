1f4aad019975bd6e2f4e73685e94eef527af2960
The-bAk-Dev <hamadi.the.music.therapist@gmail.com>
2024-04-26 18:12:35 +0200

    project almost finish no responsive aspect yet and a category js functionnality BUG

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ const sidebarItems = document.querySelectorAll('.sidebar .item');
 const catItems = document.querySelectorAll('.categories a');
 
 // Play/Pause video on mouse enter/leave
+allVideos.forEach((video) => {
+  video.addEventListener('mouseover', () => {
+    video.play();
+  });
+  video.addEventListener('mouseleave', () => {
+    video.pause();
+  });
+});
 
 // Handle sidebar item click
-
-
+sidebarItems.forEach((sideItem) => {
+  sideItem.addEventListener('click', () => {
+    sidebarItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+    sideItem.classList.add('active');
+  });
+});
 // Handle category item click
-catItems.forEach(catItem => {
-    
+// la logique est juste mais y'a quelques chose qui cloche !!
+catItems.forEach((catItem) => {
+  catItem.addEventListener('click', () => {
+    catItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+    catItem.classList.add('active');
+  });
 });
 
 // Move sidebar on small devices scroll
-window.addEventListener('scroll', () => {
-    
-});
+window.addEventListener('scroll', () => {});
