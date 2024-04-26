12463c57ec8d516f2c364fdb557869435cd34947
The-bAk-Dev <hamadi.the.music.therapist@gmail.com>
2024-04-26 04:08:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const allVideos = document.querySelectorAll('video');
+const sidebar = document.querySelector('.left-section');
+const sidebarItems = document.querySelectorAll('.sidebar .item');
+const catItems = document.querySelectorAll('.categories a');
+
+// Play/Pause video on mouse enter/leave
+
+// Handle sidebar item click
+
+
+// Handle category item click
+catItems.forEach(catItem => {
+    
+});
+
+// Move sidebar on small devices scroll
+window.addEventListener('scroll', () => {
+    
+});
